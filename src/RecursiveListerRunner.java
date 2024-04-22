5d5203afd7867ae0bf1d32a5e432f63447530ef7
Benjamin <bmckee566@gmail.com>
2024-04-22 02:33:46 -0400

    Project finish

---
+++ src/RecursiveListerRunner.java
@@ -0,0 +1,5 @@
+public class RecursiveListerRunner extends RecursiveListerFrame {
+    public static void main(String[] args) {
+        RecursiveListerFrame frame = new RecursiveListerFrame();
+    }
+}
